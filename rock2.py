cc0b16bdfc6e3b27c2e439382bf6bc02d6639077
Anjanpriyambadamahanta <42933343+Anjanpriyambadamahanta@users.noreply.github.com>
2018-10-12 12:30:08 +0530

    Add files via upload

---
+++ rock2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''import random
+
+a={1:'r',2:'p',3:'s'}
+
+while True:
+	p=input("your choice r/p/s:")
+	c=a[random.randint(1,3)]
+
+	print("your choise:",p,"computer choise:",c)
+	
+	if(p==c):
+		print('  **Draw!** ')
+	elif(p=='rock' and c=='paper'):
+		print('\t\t ## you lose! ##\t\t')
+	elif(p=='rock' and c=='scissor'):
+		print('\t\t ## you win! ##\t\t')
+	elif(p=='paper' and c=='rock'):
+		print('\t\t ## you win! ##\t\t')
+	elif(p=='paper' and c=='scissor'):
+		print('\t\t ## you lose! ##\t\t')
+	elif(p=='scissor' and c=='rock'):
+		print('\t\t ## you lose! ##\t\t')
+	elif(p=='scissor' and c=='paper'):
+		print('\t\t ## you win! ##\t\t')'''
+print("anjan")
